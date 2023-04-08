5b2decd10f5a1e42a470c66ea3d609c7658d6cb8
victor <victorluizcefet@gmail.com>
2023-04-08 09:28:57 -0300

    simple teste

--- infra/security_group.tf
+++ infra/security_group.tf
@@ -4,9 +4,9 @@ resource "aws_security_group" "geral_access" {
   ingress {
     cidr_blocks = [ "0.0.0.0/0" ]
     ipv6_cidr_blocks = [ "::/0" ]
-    from_port = 8000
-    to_port = 8000
-    protocol = "tcp"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
   }
   egress {
     cidr_blocks = [ "0.0.0.0/0" ]
